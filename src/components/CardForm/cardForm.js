35b81dff104d0faa7ecbce46326dfabb5d32903e
Monse0051 <rouge0051@gmail.com>
2019-08-05 16:31:45 -0500

    Testing fetch

--- src/components/CardForm/cardForm.js
+++ src/components/CardForm/cardForm.js
@@ -86,7 +86,7 @@ export default class App extends React.Component {
     
     const payerRequest = this.createPayerRequest(formData); 
 
-    this.postData('http://192.168.100.46:3001/pay', payerRequest)
+    this.postData('https://morning-lake-33098.herokuapp.com/pay', payerRequest)
       .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
       .catch(error => console.error(error));
  
