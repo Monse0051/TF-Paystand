2463f682f1c74ffdf25c606012b5facb75d5251f
JoannaArechiga <issassam95@hotmail.com>
2019-08-05 21:01:06 -0500

    Form style changed, handles props from Parent Component to show content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import CardForm from '../src/components/CardForm/cardForm';
 import './App.css';
+import BillingAddress from './components/BillingAddress/BillingAddress';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      visibleSection: 'card',
+      formData: null,
+    }
+
+    this.showCardForm = this.showCardForm.bind(this);
+    this.showBillingForm = this.showBillingForm.bind(this);
+  }
+
+  
+
+  showCardForm = () => {
+    this.setState ({visibleSection: 'card'}); 
+  }
+
+  showBillingForm = (payerRequest) => {
+    this.setState ({visibleSection: 'billing', payerRequest: payerRequest });
+    console.log(this.state);
+    
+    
+  }
+
+  render() {
   return (
-    <div className="App">
-      <CardForm></CardForm>
-    </div>
+    <div>
+      {this.state.visibleSection === 'card' && <CardForm showBillingForm = {this.showBillingForm}/> }
+      {this.state.visibleSection === 'billing' && <BillingAddress showCardForm = {this.showCardForm}/> }
+      </div>
   );
+}
+
 }
 
 export default App;
--- src/components/CardForm/cardForm.js
+++ src/components/CardForm/cardForm.js
@@ -9,7 +9,6 @@ import {
   formatExpirationDate,
   formatFormData,
 } from "../utils";
-
 import "react-credit-cards/es/styles-compiled.css";
 import NavBar from "../NavBar/NavBar";
 
@@ -18,17 +17,26 @@ const styles = {
 }
 
 export default class App extends React.Component {
-  state = {
-    amount: "",
-    currency: "",
-    email: "",
-    number: "",
-    name: "",
-    expiry: "",
-    cvc: "",
-    issuer: "",
-    focused: "",
-    formData: null
+  constructor(props){
+
+      super(props);
+
+            
+        this.state = {
+          amount: "",
+          currency: "",
+          email: "",
+          number: "",
+          name: "",
+          expiry: "",
+          cvc: "",
+          issuer: "",
+          focused: "",
+          formData: null,
+        
+        }
+
+
   };
 
   async postData(url = '', data = {}) {
@@ -81,11 +89,13 @@ export default class App extends React.Component {
         return acc;
       }, {});
 
-    this.setState({ formData });
-    console.log("pay button pressed!");
+    
     
     const payerRequest = this.createPayerRequest(formData); 
 
+     
+      
+      this.props.showBillingForm(formData);
     this.postData('http://192.168.100.46:3001/pay', payerRequest)
       .then(data => console.log(JSON.stringify(data))) // JSON-string from `response.json()` call
       .catch(error => console.error(error));
@@ -125,8 +135,12 @@ export default class App extends React.Component {
     };
   }
 
+
   render() {
     const { name, number, expiry, cvc, focused, issuer, formData } = this.state;
+   
+    
+
     return (
       <div key="Payment">
         <NavBar/>
@@ -200,7 +214,10 @@ export default class App extends React.Component {
             </div>
             <input type="hidden" name="issuer" value={issuer} />
             <div className="form-actions">
-              <button className="btn btn-primary btn-block">PAY</button>
+
+              
+              <button className="btn btn-primary btn-block" onClick={this.props.showBillingForm}>Go to Billing information</button>
+          
             </div>
           </form>
 
